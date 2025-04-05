fdfc4fd09f2b2938d413b9c4ad2e9286e3255b10
António Sebastião Pedro <antoniosebastiaopedro19@outlook.com>
2025-04-05 07:41:07 +0100

    adiciona a classe ClapTrap com métodos básicos e um Makefile

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 09:42:03 by ansebast          #+#    #+#             */
+/*   Updated: 2025/04/05 07:39:12 by ansebast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+	
+}
+
+ClapTrap::ClapTrap(const std::string& name)
+{
+	
+}
+
+ClapTrap::ClapTrap(const ClapTrap &clap)
+{
+	
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& clap)
+{
+	
+}
+
+ClapTrap::~ClapTrap()
+{
+	
+}
+
+
+void ClapTrap::attack(const std::string& target)
+{
+	
+}
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	
+}
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	
+}
